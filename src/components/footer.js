d2ff4d8e8b08505517d9c2ac87be2fc328ae656b
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-02-01 17:34:04 +0530

    Added Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="bg-[#353353] w-full text-center text-white py-10">
+            <div className="flex flex-row gap-5 justify-center">
+                <p>&copy; 2025 Sumit Kumar. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}   
