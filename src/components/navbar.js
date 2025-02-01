5a60fe69df12e0dd8eb0527fcec026df4b905e50
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-02-01 05:58:53 +0000

    Fixed navbar style with daisyUI components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,25 +3,30 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="fixed flex justify-end py-5 border-b-2 border-white w-full">
-      <Link href="/" className="btn btn-ghost text-xl hover:text-[#ffc107] active:text-[#ffc107]">
-        Home
-      </Link>
-      <Link href="#about" className="btn btn-ghost text-xl hover:text-[#ffc107] active:text-[#ffc107]">
-        About Me
-      </Link>
-      <a href="#skills" className="btn btn-ghost text-xl hover:text-[#ffc107] active:text-[#ffc107]">
-        Skills & Projects
-      </a>
-      <a
-        href="#experience"
-        className="btn btn-ghost text-xl hover:text-[#ffc107] active:text-[#ffc107]"
-      >
-        Experience
-      </a>
-      <a href="#contact"  className="btn btn-ghost text-xl hover:text-[#ffc107] active:text-[#ffc107]">
-        Contact Me
-      </a>
-    </nav>
+    <>
+    <div className="navbar px-60 bg-[#353353] text-white">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">Sumit Kumar</a>
+      </div>
+      <div className="navbar-end">
+        <a href="/" className="btn btn-ghost text-md hover:text-[#ffc107] active:text-[#ffc107]">
+          Home
+        </a>
+        <a href="#about" className="btn btn-ghost text-md hover:text-[#ffc107] active:text-[#ffc107]">
+          About Me
+        </a>
+        <a href="#skills" className="btn btn-ghost text-md hover:text-[#ffc107] active:text-[#ffc107]">
+          Skills & Projects
+        </a>
+        <a href="#experience" className="btn btn-ghost text-md hover:text-[#ffc107] active:text-[#ffc107]">
+          Experience
+        </a>
+        <a href="#contact" className="btn btn-ghost text-md hover:text-[#ffc107] active:text-[#ffc107]">
+          Contact Me
+        </a>
+      </div>
+    </div>
+    <div className="bg-[#353353] h-0 border-b border-white"></div>
+    </>
   );
 }
