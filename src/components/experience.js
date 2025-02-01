8c2fe3e01a0b617bdec044aefaabf7332732cde0
Sumit Kumar <kumarsumitjat298@gmail.com>
2025-02-01 19:23:23 +0530

    Added list timeline like experience component

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,10 +1,176 @@
-
 export default function Experience() {
-    return (
-        <section id="experience" className="dark:bg-[#353353] px-64 py-20">
-            <h2 className="text-4xl font-bold text-[#5E5C7F]">Experience</h2>
-            <div className="divider"></div>
-            
-        </section>
-    )
+  return (
+    <section id="experience" className="dark:bg-[#353353] px-64 py-20">
+      <h2 className="text-4xl font-bold text-[#5E5C7F]">Experience</h2>
+      <div className="divider"></div>
+
+      <div className="flex flex-row gap-10">
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical border p-[30px] rounded-xl bg-white shadow-xl w-[50%] justify-evenly">
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              <time className="text-[#8B88B1] text-sm">Aug 2023 - Present</time>
+              <div className="text-md font-black text-[#454360]">
+                Backend Developer
+              </div>
+            </div>
+            <div className="timeline-end md:mt-6 text-sm text-start px-1">
+              <p>Developed and maintaining RESTful APIs using Golang and PostgreSQL for <span className="text-warning font-bold">Kroger</span> Company.</p>
+            </div>
+            <hr />
+          </li>
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-end mb-10 md:text-start">
+              <time className="text-[#8B88B1] text-sm">
+                Feb 2023 - Aug 2023
+              </time>
+              <div className="text-lg font-black text-[#454360]">
+                Software Engineer Intern
+              </div>
+            </div>
+            <div className="timeline-content md:mt-32 text-sm text-end px-1">
+             <p>Learned about Golang and various tools and techs and worked on ZopSmart Technologies Internal products.</p>
+            </div>
+            <hr />
+          </li>
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              <time className="text-[#8B88B1] text-sm">
+                Apr 2022 - Feb 2023
+              </time>
+              <div className="text-lg font-black text-[#454360]">
+                Intern at Venom Game Studios
+              </div>
+            </div>
+            <div className="timeline-end md:mt-6 text-sm text-start px-1">
+             <p>Worked on a project using Unity game engine and contributed to a game Chor-Police (hide and seek game).</p>
+            </div>
+            <hr />
+          </li>
+        </ul>
+
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical border p-[30px] rounded-xl bg-white shadow-xl w-[50%] justify-evenly">
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              <time className="text-[#8B88B1] text-sm">
+                Jul 2022 - Aug 2022
+              </time>
+              <div className="text-lg font-black text-[#454360]">
+                Amazon ML Summer School Intern
+              </div>
+            </div>
+            <div className="timeline-end md:mt-6 text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-end mb-10 md:text-start">
+              <time className="text-[#8B88B1] text-sm">
+                Aug 2019 - Feb 2023
+              </time>
+              <div className="text-lg font-black text-[#454360]">
+                ACM Student Chapter, JUIT Solan
+              </div>
+            </div>
+            <hr />
+          </li>
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              <time className="text-[#8B88B1] text-sm">
+                Sep 2019 - Feb 2023
+              </time>
+              <div className="text-lg font-black text-[#454360]">
+                Member of Omega Leo Club, JUIT Solan
+              </div>
+            </div>
+            <hr/>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
